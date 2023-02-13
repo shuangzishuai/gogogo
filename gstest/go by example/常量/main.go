a1743c74a6c8d1317609db22f47fd520c022211d
guoshuai1 <guoshuai1@360.cn>
2023-02-13 15:04:53 +0800

    example 变量常量

---
+++ gstest/go by example/常量/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const s string = "constant"
+
+func main() {
+	fmt.Println(s)
+
+	//常数表达式可以执行任意精度的运算
+	const n = 500000000
+	const d = 3e20 / n
+	fmt.Println(d)
+
+	/**
+	数值型常量没有确定的类型，直到被给定某个类型，比如显式类型转化。
+	*/
+	fmt.Println(int64(d))
+
+	/**
+	一个数字可以根据上下文的需要（比如变量赋值、函数调用）自动确定类型。 举个例子，这里的 math.Sin 函数需要一个 float64 的参数，n 会自动确定类型。
+	*/
+	fmt.Println(math.Sin(n))
+}
