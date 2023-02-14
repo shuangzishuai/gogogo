62d26bb9ac3b7bab3539623f6ea9db226e08ef15
guoshuai1 <guoshuai1@360.cn>
2023-02-14 20:03:23 +0800

    example map

---
+++ gstest/go by example/Map类型/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := make(map[string]int)
+	m["k1"] = 7
+	m["k2"] = 10
+
+	fmt.Println(m)
+
+	v1 := m["k1"]
+	fmt.Println("v1 = ", v1)
+	fmt.Println("len = ", len(m))
+
+	delete(m, "k2")
+	fmt.Println(m)
+	_, prs := m["k2"]
+	fmt.Println("prs", prs) //false
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println(n)
+}
