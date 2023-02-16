ed24dc02170258622c32c3b6fd40595b948916b9
shuangzishuai <echoguoshuai@163.com>
2023-02-16 11:53:39 +0800

    example 函数

---
+++ gstest/go by example/ 函数/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func plus(a, b int) int {
+	return a + b
+}
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+func main() {
+	res := plus(100, 1000)
+	fmt.Println("1+2 =", res)
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+}
