0dd00e62015a4302e1086b8126a3fa26e3fc1b2e
shuangzishuai <echoguoshuai@163.com>
2023-02-20 11:16:39 +0800

    go-zero-mall User 注册logic

--- go-zero-mall/gonivinck/mall/service/user/rpc/internal/logic/registerlogic.go
+++ go-zero-mall/gonivinck/mall/service/user/rpc/internal/logic/registerlogic.go
@@ -2,7 +2,9 @@ package logic
 
 import (
 	"context"
-
+	"google.golang.org/grpc/status"
+	"mall/common/crypt"
+	"mall/service/user/model"
 	"mall/service/user/rpc/internal/svc"
 	"mall/service/user/rpc/types/user"
 
@@ -25,6 +27,36 @@ func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Register
 
 func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterResponse, error) {
 	// todo: add your logic here and delete this line
+	//判断用户是否注册
+	_, err := l.svcCtx.UserModel.FindOneByMobile(l.ctx, in.Mobile)
+	if err != nil {
+		return nil, status.Error(100, "用户已存在")
+	}
 
-	return &user.RegisterResponse{}, nil
+	if err == model.ErrNotFound {
+		newUser := model.User{
+			Name:     in.Name,
+			Gender:   in.Gender,
+			Mobile:   in.Mobile,
+			Password: crypt.PasswordEncrypt(l.svcCtx.Config.Salt, in.Password),
+		}
+		res, err := l.svcCtx.UserModel.Insert(l.ctx, &newUser)
+		if err != nil {
+			return nil, status.Error(500, err.Error())
+		}
+
+		newUser.Id, err = res.LastInsertId()
+		if err != nil {
+			return nil, status.Error(500, err.Error())
+		}
+
+		return &user.RegisterResponse{
+			Id:     newUser.Id,
+			Name:   newUser.Name,
+			Gender: newUser.Gender,
+			Mobile: newUser.Mobile,
+		}, nil
+	}
+
+	return nil, status.Error(500, err.Error())
 }
