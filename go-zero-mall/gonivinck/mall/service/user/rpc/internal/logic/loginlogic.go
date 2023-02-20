91f846d92c0a6ec3169bfd809b954042f6560fc8
shuangzishuai <echoguoshuai@163.com>
2023-02-20 10:51:04 +0800

    go-zero-mall User rpc 注册

---
+++ go-zero-mall/gonivinck/mall/service/user/rpc/internal/logic/loginlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"mall/service/user/rpc/internal/svc"
+	"mall/service/user/rpc/types/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type LoginLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *LoginLogic {
+	return &LoginLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *LoginLogic) Login(in *user.LoginRequest) (*user.LoginResponse, error) {
+	// todo: add your logic here and delete this line
+
+	return &user.LoginResponse{}, nil
+}
