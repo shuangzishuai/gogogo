e6983d4c7b2bfde0079868c2ef32a77ebdbff1da
guoshuai1 <guoshuai1@360.cn>
2023-02-25 23:37:36 +0800

    go-example 变参函数

---
+++ gstest/go by example/变参函数/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int) {
+	fmt.Println(nums, "")
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	sum(1, 2, 3, 4, 5)
+	nums := []int{1, 2, 3}
+	sum(nums...)
+}
