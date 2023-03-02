0a07bd06c0e3e73d7a5341ef03f4d78eb25c8974
guoshuai1 <guoshuai1@360.cn>
2023-03-02 14:10:45 +0800

    go-example rune

--- gstest/go by example/字符串和rune类型/main.go
+++ gstest/go by example/字符串和rune类型/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func t1() {
 	const sample = "\\xbd\\xb2\\x3d\\xbc\\x20\\xe2\\x8c\\x98"
@@ -28,6 +31,39 @@ func t1() {
 	fmt.Printf("%+q\n", sample1)
 }
 
+func example() {
+	const s = "สวัสดี"
+	fmt.Println("Len:", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+
+	fmt.Println()
+	fmt.Println("Rune count", utf8.RuneCountInString(s))
+
+	for idx, runeValue := range s {
+		fmt.Printf("%#U start at %d\n", runeValue, idx)
+	}
+
+	fmt.Println("\nUsing DecodeRuneInString")
+	for i, w := 0, 0; i < len(s); i += w {
+		runeValue, width := utf8.DecodeLastRuneInString(s[i:])
+		fmt.Printf("%#U start at %d\n", runeValue, i)
+		w = width
+		examineRune(runeValue)
+	}
+}
+
+func examineRune(r rune) {
+	if r == 't' {
+		fmt.Printf("found tee")
+	} else if r == 'ส' {
+		fmt.Println("found so sua")
+	}
+}
+
 func main() {
-	t1()
+	//t1()
+	example()
 }
