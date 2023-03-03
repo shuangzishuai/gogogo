17b91649a09fe3248b9fcb43c859d2fce4e36e6e
guoshuai1 <guoshuai1@360.cn>
2023-03-03 15:56:49 +0800

    go-example channel

--- gstest/go by example/通道同步/main.go
+++ gstest/go by example/通道同步/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func worker(done chan bool) {
+	fmt.Println("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
+
+	//程序将一直阻塞，直至收到 worker 使用通道发送的通知。
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 1)
+	go worker(done)
+	<-done
 }
