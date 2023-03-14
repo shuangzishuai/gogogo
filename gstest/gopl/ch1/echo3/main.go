ecf03b4b893db75d2cc8b46bc1525e7b53510667
guoshuai1 <guoshuai1@360.cn>
2023-03-14 11:11:32 +0800

    gopl echo3

--- gstest/gopl/ch1/echo3/main.go
+++ gstest/gopl/ch1/echo3/main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
+// 不关心格式，只想看值
 func main() {
-	fmt.Println(os.Args[0])
+	start := time.Now()
+	fmt.Println(strings.Join(os.Args[1:], "==="))
+	//fmt.Println(os.Args[0])
+	//
+	//for i, v := range os.Args {
+	//	fmt.Println(i, v)
+	//}
+	end := time.Now()
+	fmt.Println(end.Sub(start)) //26.6µs
+
 }
