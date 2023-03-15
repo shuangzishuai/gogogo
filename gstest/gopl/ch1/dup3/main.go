ed853d69849d1e965e658d6533c7aafa89ae7cb5
guoshuai1 <guoshuai1@360.cn>
2023-03-15 16:08:35 +0800

    gopl dup3

--- gstest/gopl/ch1/dup3/main.go
+++ gstest/gopl/ch1/dup3/main.go
@@ -1,1 +1,28 @@
-package dup3
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
+			continue
+		}
+		//按照换行符分割文本
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n >= 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
