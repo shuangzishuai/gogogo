cd0617cd5883fb6237eb9088732caea99707d067
guoshuai1 <guoshuai1@360.cn>
2023-03-15 15:47:37 +0800

    gopl dup2

---
+++ gstest/gopl/ch1/dup2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:] //从标准输入里面获取文件名
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		//1.打开文件 2.关闭文件
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		if n >= 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
