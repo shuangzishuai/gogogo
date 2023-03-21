93df4e353dcb49638700fdb0e3a82ee39ae2090e
guoshuai1 <guoshuai1@360.cn>
2023-03-21 22:20:30 +0800

    gopl ch3

---
+++ gstest/gopl/ch3/basename2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func basename(s string) string {
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+	return s
+}
+
+func main() {
+	s := basename("c:\\foo\\bar")
+	fmt.Println(s)
+}
