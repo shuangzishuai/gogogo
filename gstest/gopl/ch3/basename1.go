3bb7f2f5c4d2d1af0eb3bab87212cca2b1989daf
guoshuai1 <guoshuai1@360.cn>
2023-03-21 22:10:33 +0800

    gopl

---
+++ gstest/gopl/ch3/basename1.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func basename(s string) string {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	fmt.Println(s)
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func main() {
+	s := basename("a/b/c.go")
+	fmt.Println(s)
+}
