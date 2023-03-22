fc415a7092fc4eb1c10d873bfab074c94bd3284d
guoshuai1 <guoshuai1@360.cn>
2023-03-22 17:39:21 +0800

    gopl ch4 append

---
+++ gstest/gopl/ch4/append/main.go
@@ -0,0 +1,23 @@
+package main
+
+func appendInt(x []int, y int) []int {
+	var z []int
+	zlen := len(x) + 1
+	if zlen <= cap(x) {
+		z = x[:zlen]
+	} else {
+		// slice 已无空间 ，为它分配一个新的底层数组
+		zcap := zlen
+		if zcap < 2*len(x) {
+			zcap = 2 * len(x)
+		}
+		z = make([]int, zlen, zcap)
+		copy(z, x)
+	}
+	z[len(x)] = y
+	return z
+}
+
+func main() {
+
+}
