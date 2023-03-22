4649404058432cce716c0187d852adaf205d5f17
guoshuai1 <guoshuai1@360.cn>
2023-03-22 15:05:33 +0800

    gopl ch3 comma

--- gstest/gopl/ch3/comma/main.go
+++ gstest/gopl/ch3/comma/main.go
@@ -1,18 +1,21 @@
 package main
 
+import "fmt"
+
 // 函数向表示十进制非负整数的字符串中插入逗号
 //"12345",从右侧开始每三位数宇后面就插人一个逗号,形如 〝12,345”。这个版本仅对整数有效。对浮点数的处理方式留作练习。
+//TODO 使用AI提示代码
 
 func comma(s string) string {
 	n := len(s)
 	if n <= 3 {
 		return s
 	}
-	return comma(s[:n-3] + "," + s[n-3:])
+	return comma(s[:n-3]) + "," + s[n-3:]
 }
 
 func main() {
-	comma("12345")
+	fmt.Println("test comma")
+	s := comma("12345")
+	fmt.Println(s)
 }
-
-//TODO 使用AI提示代码
