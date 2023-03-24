b3f91c9b0755f246928c81cd7f443eea40ae1309
guoshuai1 <guoshuai1@360.cn>
2023-03-24 15:33:18 +0800

    gopl ch4/movies

---
+++ gstest/gopl/ch4/movie/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Movie struct {
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
+}
+
+func main() {
+	var movies = []Movie{
+		{Title: "Casablanca", Year: 1942, Color: false, Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+		{Title: "Cool Hand Luke", Year: 1967, Color: true, Actors: []string{"Paul Newman"}},
+		{Title: "Bullitt", Year: 1968, Color: true, Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+	}
+
+	fmt.Printf("%#v\n", movies)
+}
