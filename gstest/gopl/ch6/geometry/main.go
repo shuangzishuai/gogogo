ada2b22f3dffa56130cf8975b75f119834f9b274
guoshuai1 <guoshuai1@360.cn>
2023-03-31 18:18:18 +0800

    gopl ch6/geometry

--- gstest/gopl/ch6/geometry/main.go
+++ gstest/gopl/ch6/geometry/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Point struct {
 	X, Y float64
 }
+
+type Path []Point
 
 // 普通函数
 func (p Point) Distance(q Point) float64 {
@@ -14,4 +19,26 @@ func (p Point) Distance(q Point) float64 {
 // Point类型的方法
 func (p Point) Distance2(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// 路径的长度
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
+func main() {
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{6, 4},
+		// {1, 1},
+	}
+	fmt.Println(perim.Distance())
 }
