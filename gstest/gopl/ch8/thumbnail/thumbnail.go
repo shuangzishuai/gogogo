7db2f290309dee745a3d5e5e227dbc03474cfd8d
guoshuai1 <guoshuai1@360.cn>
2023-04-11 20:24:15 +0800

    gopl ch8 thumbnail

---
+++ gstest/gopl/ch8/thumbnail/thumbnail.go
@@ -0,0 +1,17 @@
+package thumbnail
+
+func ImageFile(infile string) (string, error)
+
+func makeThumbnails3(filenames []string) {
+	ch := make(chan struct{})
+	for _, f := range filenames {
+		go func(f string) {
+			thumbnail.ImageFile(f) // NOTE: ignoring errors
+			ch <- struct{}{}
+		}(f)
+	}
+	// Wait for goroutines to complete.
+	for range filenames {
+		<-ch
+	}
+}
