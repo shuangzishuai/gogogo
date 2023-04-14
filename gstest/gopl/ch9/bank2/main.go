7ec9e04dc174574e02a96f7a537909127bd888fd
guoshuai1 <guoshuai1@360.cn>
2023-04-14 15:00:30 +0800

    gopl ch9 bank

---
+++ gstest/gopl/ch9/bank2/main.go
@@ -0,0 +1,20 @@
+package main
+
+var (
+	sema    = make(chan struct{}, 1)
+	balance int
+)
+
+func Deposit(amount int) {
+	sema <- struct{}{}
+	balance += amount
+	<-sema
+}
+
+func Balance() int {
+	sema <- struct{}{}
+	b := balance
+	<-sema
+	return b
+
+}
