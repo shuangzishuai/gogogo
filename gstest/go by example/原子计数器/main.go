16a2f87312151f2684fd483dffc6ea7843fca8f4
guoshuai1 <guoshuai1@360.cn>
2023-04-17 11:39:05 +0800

    go example 原子计数器

---
+++ gstest/go by example/原子计数器/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var ops uint64 //使用无符号的整型来表示计数器
+
+	var wg sync.WaitGroup //等待所有goroutine结束
+
+	for i := 0; i < 20; i++ { //开启50个goroutine
+		wg.Add(1)
+
+		go func() {
+			for c := 0; c < 1000; c++ {
+				atomic.AddUint64(&ops, 1)
+				// ops++ //非原子性操作，最后的结果会得到不同的数字
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println("ops:", ops)
+}
