61353c60e0300814c378ff9320796a178b2e4bf9
guoshuai1 <guoshuai1@360.cn>
2023-04-17 14:37:59 +0800

    go example 原子计数器

--- gstest/go by example/原子计数器/main.go
+++ gstest/go by example/原子计数器/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
@@ -15,8 +16,11 @@ func main() {
 
 		go func() {
 			for c := 0; c < 1000; c++ {
-				// atomic.AddUint64(&ops, 1)
-				ops++ //非原子性操作，最后的结果会得到不同的数字 go run -race main.go
+				atomic.AddUint64(&ops, 1)
+				if atomic.LoadUint64(&ops)%200 == 0 {
+					fmt.Println("ops:", ops)
+				}
+				// ops++ //非原子性操作，最后的结果会得到不同的数字 go run -race main.go
 			}
 			wg.Done()
 		}()
