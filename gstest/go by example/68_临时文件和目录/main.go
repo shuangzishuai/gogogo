9a06e42795c701835c915967ddc4e48e430dc57f
guoshuai1 <guoshuai1@360.cn>
2023-04-18 17:16:41 +0800

    go example temp file

---
+++ gstest/go by example/68_临时文件和目录/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+
+	f, err := os.CreateTemp("", "sample") //创建临时文件最简单的方法是调用 os.CreateTemp 函数。 它会创建并打开文件，我们可以对文件进行读写。 函数的第一个参数传 ""，os.CreateTemp 会在操作系统的默认位置下创建该文件。
+	check(err)
+
+	fmt.Println("Temp file name:", f.Name())
+
+	defer os.Remove(f.Name())
+
+	_, err = f.Write([]byte{1, 2, 3, 4})
+	check(err)
+
+	dname, err := os.MkdirTemp("", "sampledir")
+	fmt.Println("Temp dir name:", dname)
+
+	defer os.RemoveAll(dname)
+
+	fname := filepath.Join(dname, "file1")
+	err = os.WriteFile(fname, []byte{1, 2}, 0666)
+	check(err)
+}
