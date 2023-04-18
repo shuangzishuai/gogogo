e1e8d37b5140fe590ee880696e30a6067346dc06
guoshuai1 <guoshuai1@360.cn>
2023-04-18 16:13:21 +0800

    go example line filter

---
+++ gstest/go by example/65_行过滤器/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		ucl := strings.ToUpper(scanner.Text())
+		fmt.Println(ucl)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(1)
+	}
+}
+
+//cat /tmp/lines | go run 65_行过滤器/main.go
