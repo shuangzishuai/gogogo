38286f8366afe17718c58dfaf2079a5e80d55d86
guoshuai1 <guoshuai1@360.cn>
2023-04-19 11:35:28 +0800

    go example exec command

---
+++ gstest/go by example/77_生成进程/spawning-process.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os/exec"
+)
+
+func main() {
+	dateCmd := exec.Command("date")
+
+	dateOut, err := dateCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("> date:")
+	fmt.Println(string(dateOut))
+
+	grepCmd := exec.Command("grep", "hello")
+
+	grepIn, _ := grepCmd.StdinPipe()
+	grepOut, _ := grepCmd.StdoutPipe()
+	grepCmd.Start()
+	grepIn.Write([]byte("hello grep\ngoodbye grep\n"))
+	grepIn.Close()
+	grepBytes, _ := io.ReadAll(grepOut)
+	grepCmd.Wait()
+
+	fmt.Println("> grep hello")
+	fmt.Println(string(grepBytes))
+
+	lsCmd := exec.Command("bash", "-c", "ls -alh")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("> ls -alh ")
+	fmt.Println(string(lsOut))
+}
