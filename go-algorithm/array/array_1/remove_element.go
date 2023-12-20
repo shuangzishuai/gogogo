b49f73034127086ebce8a0f096e12ca60f25ed40
shuangzishuai <echoguoshuai@163.com>
2023-12-20 11:45:02 +0800

    go-algorithm removeElement

--- go-algorithm/array/array_1/remove_element.go
+++ go-algorithm/array/array_1/remove_element.go
@@ -5,11 +5,29 @@ import "fmt"
 func main() {
 	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
 	val := 2
-	res := removeElement(nums, val)
-	fmt.Println(res)
+	// res := removeElement(nums, val)
+	// fmt.Println(res)
+
+	res1 := removeElement1(nums, val)
+	fmt.Println(res1)
 }
 
-// 双向指针
+// 双向指针法
+func removeElement1(nums []int, val int) int {
+	length := len(nums)
+	res := 0                      //慢指针
+	for i := 0; i < length; i++ { //快指针
+		if nums[i] != val {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+	nums = nums[:res]
+	fmt.Println(nums)
+	return res
+}
+
+// 相向双指针
 func removeElement(nums []int, val int) int {
 	// 有点像二分查找的左闭右闭区间 所以下面是<=
 	left := 0
