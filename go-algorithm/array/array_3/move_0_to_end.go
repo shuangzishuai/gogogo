81cb11535769477b25edf8ba802bb63221679b9a
shuangzishuai <echoguoshuai@163.com>
2024-04-25 12:05:51 +0800

    move_0_to_end

--- go-algorithm/array/array_3/move_0_to_end.go
+++ go-algorithm/array/array_3/move_0_to_end.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 	输入: nums = [0,1,0,3,12]
 	输出: [1,3,12,0,0]
@@ -7,9 +9,19 @@ package main
 
 func main() {
 	nums := []int{0, 1, 0, 3, 12}
-	moveZeroes(nums)
+	lens := moveZeroes(nums, 0)
+	fmt.Printf("%v\n", nums[:lens])
 }
 
-func moveZeroes(nums []int) {
+func moveZeroes(nums []int, val int) int {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] == val {
+			continue
+		}
+		nums[slow] = nums[fast]
+		slow++
+	}
 
+	return slow
 }
