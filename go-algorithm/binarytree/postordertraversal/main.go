00c91ee4c028a941b95d68e3d0f9019a4564ca52
shuangzishuai <echoguoshuai@163.com>
2025-01-14 11:59:46 +0800

    postorder by stack

--- go-algorithm/binarytree/postordertraversal/main.go
+++ go-algorithm/binarytree/postordertraversal/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 type TreeNode struct {
 	Val   int
@@ -8,6 +11,7 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// 递归实现
 func postTraversal(root *TreeNode) (res []int) {
 	var traversal func(node *TreeNode)
 	traversal = func(node *TreeNode) {
@@ -22,6 +26,40 @@ func postTraversal(root *TreeNode) (res []int) {
 	return res
 }
 
+// 迭代实现
+func postTraversalByStack(root *TreeNode) []int {
+	var res []int
+
+	if root == nil {
+		return res
+	}
+	st := list.New()
+	st.PushBack(root)
+
+	for st.Len() > 0 {
+		node := st.Remove(st.Back()).(*TreeNode)
+		res = append(res, node.Val)
+		if node.Left != nil {
+			st.PushBack(node.Left)
+		}
+		if node.Right != nil {
+			st.PushBack(node.Right)
+		}
+	}
+	reverse(res)
+
+	return res
+}
+
+func reverse(nums []int) {
+	l, r := 0, len(nums)-1
+	for l < r {
+		nums[l], nums[r] = nums[r], nums[l]
+		l++
+		r--
+	}
+}
+
 func main() {
 	var root TreeNode
 	root.Val = 1
@@ -29,5 +67,6 @@ func main() {
 	root.Right = &TreeNode{3, nil, nil}
 	root.Left.Left = &TreeNode{4, nil, nil}
 	root.Left.Right = &TreeNode{5, nil, nil}
-	fmt.Println(postTraversal(&root)) // [4 5 2 3 1]
+	fmt.Println(postTraversal(&root))        // [4 5 2 3 1]
+	fmt.Println(postTraversalByStack(&root)) // [4 5 2 3 1]
 }
