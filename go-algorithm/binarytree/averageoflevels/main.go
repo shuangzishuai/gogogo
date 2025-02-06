fa6b18a536f8d087fb603f812f5801752879ddfc
shuangzishuai <echoguoshuai@163.com>
2025-02-06 15:30:12 +0800

    average of levels

--- go-algorithm/binarytree/averageoflevels/main.go
+++ go-algorithm/binarytree/averageoflevels/main.go
@@ -1,15 +1,65 @@
 package main
 
+import "container/list"
+
 type TreeNode struct {
-	Val int
-	LeftNode *TreeNode
-	RightNode *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
-
 func averageOfLevels(root *TreeNode) []float64 {
+	if root == nil {
+		return nil
+	}
 
+	res := make([]float64, 0)
+	queue := list.New()
+	queue.PushBack(root)
+
+	var sum int
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+			sum += node.Val
+		}
+
+		res = append(res, float64(sum)/float64(length))
+		sum = 0
+	}
+
+	return res
+
+}
 
 func main() {
-
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	averageOfLevels(root)
 }
