b798db3662f44818d6101fe62c05932a8255cbd8
shuangzishuai <echoguoshuai@163.com>
2025-02-06 15:03:18 +0800

    right side traversal

---
+++ go-algorithm/binarytree/rightsidetraversal/main.go
@@ -0,0 +1,59 @@
+package main
+
+import "testing/quick"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		retrun nil
+	}
+	res := make([]int, 0)
+	queue := list.New()
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		for i := 0; i < length; i++ {
+			node := queue.Remove(queue.Front).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+			if i == length-1 {
+				res = append(res, node.Val)
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	root := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 4,
+			},
+			Right: &TreeNode{
+				Val: 5,
+			},
+		},
+		Right: &TreeNode{
+			Val: 3,
+			Left: &TreeNode{
+				Val: 6,
+			},
+			Right: &TreeNode{
+				Val: 7,
+			},
+		},
+	}
+	rightSideView(root)
+}
