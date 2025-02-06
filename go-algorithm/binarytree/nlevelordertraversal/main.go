b4fe68cbe22552f5147c51299553cafd009c0755
shuangzishuai <echoguoshuai@163.com>
2025-02-06 16:53:34 +0800

    N levels traversal

---
+++ go-algorithm/binarytree/nlevelordertraversal/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+)
+
+type rootNode struct {
+	Val      int
+	Children []*rootNode
+}
+
+func levelorder(root *rootNode) [][]int {
+	queue := list.New()
+	res := [][]int{}
+	if root == nil {
+		return res
+	}
+	queue.PushBack(root)
+	for queue.Len() > 0 {
+		length := queue.Len()
+		var tmp []int
+		for T := 0; T < length; T++ {
+			myNode := queue.Remove(queue.Front()).(*rootNode)
+			tmp = append(tmp, myNode.Val)
+			for i := 0; i < len(myNode.Children); i++ {
+				queue.PushBack(myNode.Children[i])
+			}
+		}
+		res = append(res, tmp)
+		fmt.Printf("res = %v", res)
+	}
+
+	return res
+}
+
+func main() {
+	rootNode := &rootNode{
+		Val: 1,
+		Children: []*rootNode{
+			{
+				Val: 3,
+				Children: []*rootNode{
+					{
+						Val: 5,
+					},
+					{
+						Val: 6,
+					},
+				},
+			},
+			{
+				Val: 2,
+			},
+			{
+				Val: 4,
+			},
+		},
+	}
+	levelorder(rootNode)
+}
